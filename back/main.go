f03a95cdf1b1d3db8dba39965a54184d268a955a
MonkeyBusiness <gutorov.artem@yandex.ru>
2020-04-06 17:58:08 +0300

    initial commit

---
+++ back/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+type answer struct {
+	Date time.Time `json:"time"`
+	Name string    `json:"name"`
+	A1   string    `json:"a1"`
+	A2   string    `json:"a2"`
+	A3   string    `json:"a3"`
+	A4   string    `json:"a4"`
+	A5   string    `json:"a5"`
+}
+
+func main() {}
